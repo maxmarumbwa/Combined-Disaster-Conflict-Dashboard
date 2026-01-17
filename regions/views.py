c8e3f90637619befccf7ec26928f92e3956904cc
maxmarumbwa <maxmarumbwa@gmail.com>
2026-01-17 23:05:25 +0200

    list all admin and convert then to links redirect to admin code

--- regions/views.py
+++ regions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from regions.models import adm1
 
-# Create your views here.
+
+def index(request):
+    adm_1 = adm1.objects.all()
+    context = {}
+    context["adm1"] = adm_1
+    return render(request, "index.html", context)
