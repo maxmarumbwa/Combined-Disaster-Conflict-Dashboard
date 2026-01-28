7f1a964d80bde93287f1c125453cb043f95bf6df
maxmarumbwa <maxmarumbwa@gmail.com>
2026-01-28 13:35:26 +0200

    NEW- choropleth map with async fn

--- disaster_conflict/settings.py
+++ disaster_conflict/settings.py
@@ -16,8 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
-print("BASE_DIR fm =", BASE_DIR)
-print("STATIC DIR fm =", BASE_DIR / "static")
+# print("BASE_DIR fm =", BASE_DIR)
+# print("STATIC DIR fm =", BASE_DIR / "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
